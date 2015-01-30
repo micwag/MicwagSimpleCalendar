f76f04fc297294f508e35d9e3a413055ac81051a
Michael Wagner <michael-wagner@micwag.de>
2015-01-30 12:54:14 +0100

    Added tag functionality to widget frontend

--- lib/MicwagSimpleCalendarWidgetList.php
+++ lib/MicwagSimpleCalendarWidgetList.php
@@ -58,79 +58,122 @@ class MicwagSimpleCalendarWidgetList extends WP_Widget {
 			$dates_final = $dates;
 		}
 
-		echo $this->generate_html( $dates );
+		$appointmentsContent = $this->generate_html( $instance, $dates_final );
+
+		if ( isset( $instance['widgetContent'] ) ) {
+			$widgetContent = $instance['widgetContent'];
+		} else {
+			$widgetContent = '%appointments%';
+		}
+
+		$widgetContent = str_replace( '%appointments%', $appointmentsContent, $widgetContent );
+		echo $widgetContent;
 
 		echo $args['after_widget'];
 	}
 
-	public function generate_html( $dates ) {
-		$html = "<ol class=\"date-list\">";
-		foreach ( $dates as $date ) {
-			$id        = $date['id'];
-			$beginning = $date['beginning'];
-			$datetime  = date_create( $beginning );
-			$title     = $date['title'];
-
-			$html .= "<li>";
-			$html .= "<time datetime=\"" . $beginning . "\">";
-			$html .= date_format( $datetime, "d. " ) . $this->months[ intval( date_format( $datetime, 'm' ) ) ];
-			$html .= "</time> ";
-			$html .= $title;
-			$html .= "</li>";
+	/**
+	 * Returns the markup for the appointments
+	 *
+	 * @param array $instance
+	 * @param       $dates
+	 *
+	 * @return string
+	 */
+	public function generate_html( $instance, $dates ) {
+		$appointmentsContent = "";
+		if ( isset( $instance['appointmentContent'] ) ) {
+			$appointmentMarkup = $instance['appointmentContent'];
+		} else {
+			$appointmentMarkup = '<article><h1><a href="%appointment_permalink%">%appointment_title%</a></h1></article>';
 		}
-		$html .= "</ol>";
 
-		return $html;
+		foreach ( $dates as $date ) {
+			$appointmentContent = $appointmentMarkup;
+			$appointmentContent = str_replace( '%appointment_title%', $date['title'], $appointmentContent );
+			$appointmentContent = str_replace( '%appointment_id%', $date['id'], $appointmentContent );
+			$appointmentContent = str_replace( '%appointment_beginning%', $date['beginning'], $appointmentContent );
+			$appointmentContent = str_replace( '%appointment_end%', $date['end'], $appointmentContent );
+			$appointmentContent = str_replace( '%appointment_description%', $date['description'], $appointmentContent );
+			$appointmentContent = str_replace( '%appointment_location%', $date['location'], $appointmentContent );
+
+			$appointmentsContent .= $appointmentContent;
+		}
+
+		return $appointmentsContent;
 	}
 
 	public function form( $instance ) {
 
-		// Title field
+		// Title
 		if ( isset( $instance['title'] ) ) {
 			$title = $instance['title'];
 		} else {
 			$title = __( 'Dates', 'micwag-simple-calendar' );
 		}
-		echo "<p>";
-		echo "<label for='" . $this->get_field_id( 'title' );
-		echo "'>" . __( 'Title', 'ranger-calendar' ) . "</label> ";
+		echo '<p>';
+		echo '<label for="' . $this->get_field_id( 'title' );
+		echo '">' . __( 'Title', 'ranger-calendar' ) . '</label>';
 
-		echo "<input type='text' id='" . $this->get_field_id( 'title' );
-		echo "' name='" . $this->get_field_name( 'title' );
-		echo "' value='" . esc_attr( $title ) . "' />";
+		echo '<input class="widefat" type="text" id="' . $this->get_field_id( 'title' );
+		echo '" name="' . $this->get_field_name( 'title' );
+		echo '" value="' . esc_attr( $title ) . '" />';
 		echo "</p>";
 
-		// Category field
+		// Category
 		if ( isset( $instance['category'] ) ) {
 			$category = $instance['category'];
 		} else {
 			$category = 0;
 		}
 		$categories = $this->calendar->get_categories();
-		echo "<p>";
-		echo "<label for='" . $this->get_field_id( 'category' );
-		echo "'>" . __( 'Category', 'micwag-simple-calendar' ) . "</label> ";
+		echo '<p>';
+		echo '<label for="' . $this->get_field_id( 'category' );
+		echo '">' . __( 'Category', 'micwag-simple-calendar' ) . '</label>';
 
-		echo "<select name='" . $this->get_field_name( 'category' );
-		echo "' id='" . $this->get_field_id( 'category' );
-		echo "'>";
+		echo '<select class="widefat" name="' . $this->get_field_name( 'category' );
+		echo '" id="' . $this->get_field_id( 'category' );
+		echo '">';
 
-		echo "<option value='0'";
+		echo '<option value="0"';
 		if ( $instance['category'] == '0' ) {
-			echo " selected";
+			echo ' selected';
 		}
-		echo ">" . __( 'None', 'micwag-simple-calendar' ) . "</option>";
+		echo '>' . __( 'None', 'micwag-simple-calendar' ) . '</option>';
 
 		foreach ( $categories as $category ) {
-			echo "<option value='" . $category['id'] . "'";
+			echo '<option value="' . $category['id'] . '"';
 			if ( $category['id'] == $instance['category'] ) {
-				echo " selected";
+				echo ' selected';
 			}
-			echo ">" . $category['name'] . "</option>";
+			echo '>' . $category['name'] . '</option>';
 		}
 
-		echo "</select>";
-		echo "</p>";
+		echo '</select>';
+		echo '</p>';
+
+		// Widget Content
+		if ( isset( $instance['widgetContent'] ) ) {
+			$widgetContent = $instance['widgetContent'];
+		} else {
+			$widgetContent = '%appointments%';
+		}
+		echo '<p><label for="' . $this->get_field_id( 'widgetContent' ) . '">' . __( 'Widget content',
+				'wp-last-posts-widget' ) . ': </label>';
+		echo '<textarea class="widefat" id="' . $this->get_field_id( 'widgetContent' ) . '" name="'
+		     . $this->get_field_name( 'widgetContent' ) . '">' . esc_attr( $widgetContent ) . '</textarea></p>';
+
+		// Appointment Content
+		if ( isset( $instance['appointmentContent'] ) ) {
+			$appointmentContent = $instance['appointmentContent'];
+		} else {
+			$appointmentContent = '<article><h1><a href="%appointment_permalink%">%appointment_title%</a></h1></article>';
+		}
+		echo '<p><label for="' . $this->get_field_id( 'appointmentContent' ) . '">' . __( 'Appointment content',
+				'wp-last-posts-widget' ) . ': </label>';
+		echo '<textarea class="widefat" id="' . $this->get_field_id( 'appointmentContent' ) . '" name="'
+		     . $this->get_field_name( 'appointmentContent' ) . '">' . esc_attr( $appointmentContent )
+		     . '</textarea></p>';
 	}
 
 	public function update( $new_instance, $old_instance ) {
@@ -140,23 +183,43 @@ class MicwagSimpleCalendarWidgetList extends WP_Widget {
 			$instance = array();
 		}
 
-		if(isset($new_instance['title'])) {
+		if ( isset( $new_instance['title'] ) ) {
 			$instance['title'] = $new_instance['title'];
 		} else {
-			if(isset($old_instance['title'])) {
+			if ( isset( $old_instance['title'] ) ) {
 				$instance['title'] = $old_instance['title'];
 			} else {
 				$instance['title'] = '';
 			}
 		}
 
-		if(isset($new_instance['category'])) {
+		if ( isset( $new_instance['category'] ) ) {
 			$instance['category'] = $new_instance['category'];
 		} else {
-			if(isset($old_instance['category'])) {
+			if ( isset( $old_instance['category'] ) ) {
 				$instance['category'] = $old_instance['category'];
 			} else {
 				$instance['category'] = '';
+			}
+		}
+
+		if ( isset( $new_instance['widgetContent'] ) ) {
+			$instance['widgetContent'] = $new_instance['widgetContent'];
+		} else {
+			if ( isset( $old_instance['widgetContent'] ) ) {
+				$instance['widgetContent'] = $old_instance['widgetContent'];
+			} else {
+				$instance['widgetContent'] = '';
+			}
+		}
+
+		if ( isset( $new_instance['appointmentContent'] ) ) {
+			$instance['appointmentContent'] = $new_instance['appointmentContent'];
+		} else {
+			if ( isset( $old_instance['appointmentContent'] ) ) {
+				$instance['appointmentContent'] = $old_instance['appointmentContent'];
+			} else {
+				$instance['appointmentContent'] = '';
 			}
 		}
 
